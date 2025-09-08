38eba9dc71712c9526928772e939c9a808bad449
tuanhao2003 <nhtuanhao@gmail.com>
2025-09-08 16:39:57 +0700

    refactor: conversation, accountConversation repo

---
+++ app/repositories/accountConversationRepo.py
@@ -0,0 +1,130 @@
+import uuid
+from django.utils.timezone import datetime, now
+from app.entities.accountConversation import AccountConversation
+from app.entities.account import Account
+from app.entities.conversation import Conversation
+from django.core.paginator import Paginator
+
+
+class AccountsConversationsRepo:
+    @staticmethod
+    def all(page: int, page_size: int, is_active: bool | None):
+        try:
+            if is_active is None:
+                queryset = AccountConversation.objects.select_related("account", "conversation").all().order_by("-created_at")
+            else:
+                queryset = AccountConversation.objects.select_related("account", "conversation").filter(is_active=is_active).order_by("-created_at")
+
+            paginator = Paginator(queryset, page_size)
+            items = paginator.page(page)
+
+            return {
+                "pages": paginator.num_pages,
+                "current": items.number,
+                "content": list(items),
+            }
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def find_by_id(accountConversation_id: uuid.UUID, is_active: bool | None):
+        try:
+            if is_active is None:
+                return AccountConversation.objects.select_related("account", "conversation").get(id=accountConversation_id)
+            return AccountConversation.objects.select_related("account", "conversation").get(id=accountConversation_id, is_active=is_active)
+        except Conversation.DoesNotExist:
+            return None
+        except Exception as e:
+            raise e
+        
+    @staticmethod
+    def find_by_account(account: Account, page:int, page_size: int, is_active: bool | None):
+        try:
+            if is_active is None:
+                queryset = AccountConversation.objects.select_related("account", "conversation").filter(account=account).order_by("-last_accessed")
+            else:
+                queryset = AccountConversation.objects.select_related("account", "conversation").filter(account=account, is_active=is_active).order_by("-last_accessed")
+
+            paginator = Paginator(queryset, page_size)
+            items = paginator.page(page)
+
+            return {
+                "pages": paginator.num_pages,
+                "current": items.number,
+                "content": list(items),
+            }
+        except Exception as e:
+            raise e
+        
+    @staticmethod
+    def find_by_conversation(conversation: Conversation, page:int, page_size: int, is_active: bool | None):
+        try:
+            if is_active is None:
+                queryset = AccountConversation.objects.select_related("account", "conversation").filter(conversation=conversation).order_by("-last_accessed")
+            else:
+                queryset = AccountConversation.objects.select_related("account", "conversation").filter(conversation=conversation, is_active=is_active).order_by("-last_accessed")
+
+            paginator = Paginator(queryset, page_size)
+            items = paginator.page(page)
+
+            return {
+                "pages": paginator.num_pages,
+                "current": items.number,
+                "content": list(items),
+            }
+        except Exception as e:
+            raise e
+        
+    @staticmethod
+    def find_by_both(account: Account, conversation: Conversation, is_active: bool | None):
+        try:
+            if is_active is None:
+                return AccountConversation.objects.select_related("account", "conversation").get(conversation=conversation, account=account)
+            return AccountConversation.objects.select_related("account", "conversation").get(conversation=conversation, account=account, is_active=is_active)
+        except Conversation.DoesNotExist:
+            return None
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def handle_create(data: dict):
+        try:
+            return AccountConversation.objects.create(**data)
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def handle_update(accountConversation: AccountConversation, data: dict):
+        try:
+            for field, value in data.items():
+                setattr(accountConversation, field, value)
+            accountConversation.save(update_fields=list(data.keys()))
+            return accountConversation
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def handle_delete(accountConversation: AccountConversation):
+        try:
+            accountConversation.is_active = False
+            accountConversation.save(update_fields=["is_active"])
+            return accountConversation
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def handle_hard_delete(accountConversation: AccountConversation):
+        try:
+            accountConversation.delete()
+            return True
+        except Exception as e:
+            raise e
+
+    @staticmethod
+    def handle_update_last_accessed(accountConversation: AccountConversation):
+        try:
+            accountConversation.last_accessed = now()
+            accountConversation.save()
+            return accountConversation
+        except Exception as e:
+            raise e
--- app/repositories/conversationRepo.py
+++ app/repositories/conversationRepo.py
@@ -1,7 +1,9 @@
 import uuid
-from django.utils.timezone import datetime, now
+from app.entities.account import Account
 from app.entities.conversation import Conversation
 from django.core.paginator import Paginator
+
+from app.enums.conversationTypes import ConversationTypes
 
 
 class ConversationRepo:
@@ -21,113 +23,99 @@ class ConversationRepo:
                 "current": items.number,
                 "content": list(items),
             }
-        except Exception:
-            return None
+        except Exception as e:
+            raise e
         
     @staticmethod
-    def find_by_id(account_id: uuid.UUID, is_active: bool | None):
+    def find_by_id(conversation_id: uuid.UUID, is_active: bool | None):
         try:
             if is_active is None:
-                return Conversation.objects.select_related("creator").get(id=account_id)
-            return Conversation.objects.select_related("creator").get(id=account_id, is_active=is_active)
-        except Account.DoesNotExist:
+                return Conversation.objects.select_related("creator").get(id=conversation_id)
+            return Conversation.objects.select_related("creator").get(id=conversation_id, is_active=is_active)
+        except Conversation.DoesNotExist:
             return None
-
+        except Exception as e:
+            raise e
+    
     @staticmethod
-    def get_by_id(conversation_id: uuid.UUID):
+    def find_by_title(title: str, page:int, page_size: int, is_active: bool | None):
         try:
-            return Conversation.objects.select_related("creator").get(id=conversation_id)
-        except Exception:
-            return None
+            if is_active is None:
+                queryset = Conversation.objects.select_related("creator").filter(title=title).order_by("-created_at")
+            else:
+                queryset = Conversation.objects.select_related("creator").filter(title=title, is_active=is_active).order_by("-created_at")
+
+            paginator = Paginator(queryset, page_size)
+            items = paginator.page(page)
+
+            return {
+                "pages": paginator.num_pages,
+                "current": items.number,
+                "content": list(items),
+            }
+        except Exception as e:
+            raise e
+    
+    @staticmethod
+    def find_by_type(type: ConversationTypes.choices ,page:int, page_size: int, is_active: bool | None):
+        try:
+            if is_active is None:
+                queryset = Conversation.objects.select_related("creator").filter(type=type).order_by("-created_at")
+            else:
+                queryset = Conversation.objects.select_related("creator").filter(type=type, is_active=is_active).order_by("-created_at")
+
+            paginator = Paginator(queryset, page_size)
+            items = paginator.page(page)
+
+            return {
+                "pages": paginator.num_pages,
+                "current": items.number,
+                "content": list(items),
+            }
+        except Exception as e:
+            raise e
         
     @staticmethod
-    def filter_by_title(title: str):
+    def find_by_creator(creator: Account, is_active: bool | None):
         try:
-            return Conversation.objects.select_related("creator").filter(title__icontains=title)
-        except Exception:
+            if is_active is None:
+                return Conversation.objects.select_related("creator").get(creator=creator)
+            return Conversation.objects.select_related("creator").get(creator=creator, is_active=is_active)
+        except Conversation.DoesNotExist:
             return None
+        except Exception as e:
+            raise e
 
     @staticmethod
-    def get_all_personal_chats():
+    def handle_create(data: dict):
         try:
-            return Conversation.objects.select_related("creator").filter(is_group=False, is_community=False)
-        except Exception:
-            return None
-        
-    @staticmethod
-    def get_all_groups(isgroup: bool):
-        try:
-            return Conversation.objects.select_related("creator").filter(is_group=isgroup)
-        except Exception:
-            return None
-        
-    @staticmethod
-    def get_all_communities(iscommunity: bool):
-        try:
-            return Conversation.objects.select_related("creator").filter(is_community=iscommunity)
-        except Exception:
-            return None
-            
-    @staticmethod
-    def get_by_creator(creator: Accounts):
-        try:
-            return Conversation.objects.select_related("creator").get(creator=creator)
-        except Exception:
-            return None
+            return Conversation.objects.create(**data)
+        except Exception as e:
+            raise e
 
     @staticmethod
-    def filter_by_date_created(date: datetime):
-        try:
-            return Conversation.objects.select_related("creator").filter(created_at=date)
-        except Exception:
-            return None
-
-    @staticmethod
-    def filter_by_status(status: bool = True):
-        try:
-            return Conversation.objects.select_related("creator").filter(is_active=status)
-        except Exception:
-            return None
-
-    @staticmethod
-    def filter_by_birth_day(date: datetime):
-        try:
-            return Conversation.objects.select_related("creator").filter(birth = date)
-        except Exception:
-            return None
-
-    @staticmethod
-    def do_create(data: dict):
-        try:
-            return Conversation.objects.select_related("creator").create(**data)
-        except Exception:
-            return None
-
-    @staticmethod
-    def do_update(conversation: Conversation, data: dict):
+    def handle_update(conversation: Conversation, data: dict):
         try:
             for field, value in data.items():
                 setattr(conversation, field, value)
-            conversation.updated_at = now()
-            conversation.save(update_fields=data.keys()) 
+            conversation.save(update_fields=list(data.keys()))
             return conversation
-        except Exception:
-            return None
+        except Exception as e:
+            raise e
 
     @staticmethod
-    def do_delete(conversation: Conversation):
+    def handle_delete(conversation: Conversation):
         try:
             conversation.is_active = False
-            conversation.updated_at = now()
-            conversation.save()
+            conversation.save(update_fields=["is_active"])
             return conversation
-        except Exception:
-            return None
+        except Exception as e:
+            raise e
 
     @staticmethod
-    def do_hard_delete(conversation: Conversation):
+    def handle_hard_delete(conversation: Conversation):
         try:
             conversation.delete()
             return True
-        except Exception:
-            return False
+        except Exception as e:
+            raise e
