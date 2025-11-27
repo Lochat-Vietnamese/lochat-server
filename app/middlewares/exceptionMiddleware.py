207a59cdaaba48240e911a4d07c77a0e7c6ff6cc
tuanhao2003 <nhtuanhao@gmail.com>
2025-11-27 23:46:08 +0700

    feat: add exception middleware and use it in app

---
+++ app/middlewares/exceptionMiddleware.py
@@ -0,0 +1,19 @@
+from rest_framework.exceptions import APIException
+from django.utils.deprecation import MiddlewareMixin
+from app.utils.baseResponse import BaseResponse
+
+class ExceptionMiddleware(MiddlewareMixin):
+    def process_exception(self, request, exception):
+        if isinstance(exception, APIException):
+            status_code = exception.status_code
+            detail = exception.detail
+
+            if isinstance(detail, dict):
+                if 'detail' in detail:
+                    message = detail['detail']
+                else:
+                    message = detail
+            else:
+                message = str(detail)            
+            return BaseResponse.send(status_code=status_code, message=message)
+        return None
