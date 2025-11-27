1cf694f737abe35ab75c0b00217a7b14cdab64ca
tuanhao2003 <nhtuanhao@gmail.com>
2025-11-28 00:05:05 +0700

    refactor(account): use exception helper in controller

--- app/controllers/accountController.py
+++ app/controllers/accountController.py
@@ -1,33 +1,33 @@
 from django.views import View
 
+from app.enums.responseMessages import ResponseMessages
 from app.services.accountService import AccountService
 from app.mapping.accountMapping import AccountMapping
 from app.utils.baseResponse import BaseResponse
-from app.utils.logHelper import LogHelper
+from app.utils.exceptionHelper import ExceptionHelper
 from app.utils.parseBool import ParseBool
 from app.utils.requestData import RequestData
+
 
 class AccountController(View):
     async def post(self, request, action=None):
         try:
             data = RequestData(request=request)
 
-
             if action == "get-by-id":
                 account_id = data.get("account_id")
                 is_active = ParseBool(data.get("is_active", "true"))
 
-                if account_id:
-                    result = await AccountService.get_by_id(
-                        str(account_id), is_active=is_active
-                    )
-                    if result:
-                        return BaseResponse.success(data=AccountMapping(result).data)
-                    return BaseResponse.error(message="process_failed")
-                return BaseResponse.error()
-            
+                if not account_id:
+                    ExceptionHelper.throw_bad_request(ResponseMessages.MISSING_DATA)
 
-            return BaseResponse.error(message="invalid_endpoint")
+                result = await AccountService.get_by_id( str(account_id), is_active=is_active)
+            
+                if not result:
+                    ExceptionHelper.throw_not_found(ResponseMessages.NOT_FOUND)
+                    
+                return BaseResponse.send(data=AccountMapping(result).data)
+
+            ExceptionHelper.throw_bad_request(ResponseMessages.INVALID_ENDPOINT)
         except Exception as e:
-            LogHelper.error(message=str(e))
-            return BaseResponse.internal(data=str(e))
+            ExceptionHelper.handle_caught_exception(error=e)
--- app/enums/responseMessages.py
+++ app/enums/responseMessages.py
@@ -3,8 +3,10 @@ from enum import Enum
 class ResponseMessages(str, Enum):
     UNEXPECTED_ERROR = "unexpected_error"
     BAD_REQUEST = "bad_request"
-    NOT_FOUND = "not_found"
+    NOT_FOUND = "data_not_found"
     UNAUTHORIZED = "unauthorized"
     FORBIDDEN = "forbidden"
     SERVER_ERROR = "server_error"
     SUCCESS = "success"
+    INVALID_ENDPOINT = "invalid_endpoint"
+    MISSING_DATA = "missing_data"
