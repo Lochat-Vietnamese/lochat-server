3e97c1e115d5d6df4238751b69792436e0a7c1ff
tuanhao2003 <nhtuanhao@gmail.com>
2025-12-30 13:14:47 +0700

    refactor(profile): apply dto in controller and service functions

--- app/controllers/profileController.py
+++ app/controllers/profileController.py
@@ -1,12 +1,12 @@
 from django.views import View
 
+from app.dtos.profileDTOs import GetAllProfileDTO, GetProfileByIdDTO
 from app.enums.responseMessages import ResponseMessages
 from app.mapping.profileMapping import ProfileMapping
 from app.services.accountService import AccountService
 from app.services.profileService import ProfileService
 from app.utils.baseResponse import BaseResponse
 from app.utils.exceptionHelper import ExceptionHelper
-from app.utils.logHelper import LogHelper
 from app.utils.parseBool import ParseBool
 from app.utils.requestData import RequestData
 
@@ -16,42 +16,38 @@ class ProfileController(View):
             data = RequestData(request=request)
                 
             if action == "all-users":
-                page = data.get("page", "1")
-                page_size = data.get("page_size", "20")
-                is_active = ParseBool(data.get("is_active", "True"))
+                dto = GetAllProfileDTO(**data)
 
-                result = await ProfileService.get_all(page=int(page), page_size=int(page_size), is_active=is_active)
+                result = await ProfileService.get_all(dto)
                 result["content"] = ProfileMapping(result.get("content", []), many=True).data
                 return BaseResponse.send(data=result)
             
             if action == "get-by-id":
-                profile_id = data.get("profile_id", None)
-                is_active = ParseBool(data.get("is_active", "True"))
-                if profile_id:
-                    result = await ProfileService.get_by_id(profile_id=profile_id, is_active=is_active)
-                    return BaseResponse.send(data=ProfileMapping(result).data)
-                ExceptionHelper.throw_bad_request(ResponseMessages.MISSING_DATA)
+                dto = GetProfileByIdDTO(**data)
+                result = await ProfileService.get_by_id(dto)
+                return BaseResponse.send(data=ProfileMapping(result).data)
 
-            if action == "search":
-                search_data = data.get("search_data")
-                page = int(data.get("page", "1"))
-                page_size = int(data.get("page_size", "20"))
-                is_active = ParseBool(data.get("is_active", "True"))
+# làm lại sau khi tổ chức restful api
+            # if action == "search":
+            #     search_data = data.get("search_data")
+            #     page = int(data.get("page", "1"))
+            #     page_size = int(data.get("page_size", "20"))
+            #     is_active = ParseBool(data.get("is_active", "True"))
 
-                if search_data:
-                    result = None
-                    if AccountService.is_valid_email(str(search_data)):
-                        account = await AccountService.get_by_email(email=search_data, is_active=is_active)
-                        result = account.profile
-                    if not result:
-                        result = await ProfileService.get_by_nickname(nickname=search_data, page=page, page_size=page_size, is_active=is_active)
+            #     if search_data:
+            #         result = None
+            #         if AccountService.is_valid_email(str(search_data)):
+            #             account = await AccountService.get_by_email(email=search_data, is_active=is_active)
+            #             result = account.profile
+            #         if not result:
+            #             result = await ProfileService.get_by_nickname(nickname=search_data, page=page, page_size=page_size, is_active=is_active)
 
-                    if result:
-                        if isinstance(result, dict):
-                            result["content"] = ProfileMapping(result.get("content", []), many=True).data
-                            return BaseResponse.send(data=result)
-                        return BaseResponse.send(data=ProfileMapping(result).data)
-                ExceptionHelper.throw_bad_request(ResponseMessages.MISSING_DATA)
+            #         if result:
+            #             if isinstance(result, dict):
+            #                 result["content"] = ProfileMapping(result.get("content", []), many=True).data
+            #                 return BaseResponse.send(data=result)
+            #             return BaseResponse.send(data=ProfileMapping(result).data)
+            #     ExceptionHelper.throw_bad_request(ResponseMessages.MISSING_DATA)
                 
 
             ExceptionHelper.throw_bad_request(ResponseMessages.INVALID_ENDPOINT)
---
+++ app/dtos/profileDTOs.py
@@ -0,0 +1,44 @@
+from uuid import UUID
+from pydantic import Field, field_validator
+from app.utils.parseBool import ParseBool
+from app.dtos.baseDTO import BaseDTO
+
+
+class GetAllProfileDTO(BaseDTO):
+    page: int = Field(
+        title="Current Page",
+        ge=1,
+        default=1, 
+        examples=1
+    ),
+    page_size: int = Field(
+        title="Page Size",
+        ge=5,
+        lt=100,
+        default=10, 
+        examples=10
+    ),
+    is_active: bool = Field(
+        title="Activity Status",
+        default=True, 
+        examples=True
+    )
+
+    @field_validator("is_active", mode="before")
+    def parse_is_active(cls, input):
+        return ParseBool(input)
+
+class GetProfileByIdDTO(BaseDTO):
+    profile_id: UUID = Field(
+        title="Profile ID",
+        example="123e4567-e89b-12d3-a456-426655440000",
+    ),
+    is_active: bool = Field(
+        title="Activity Status",
+        default=True, 
+        examples=True
+    )
+
+    @field_validator("is_active", mode="before")
+    def parse_is_active(cls, input):
+        return ParseBool(input)
