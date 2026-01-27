36bec8ee4daeef9e3793530a056533f614b7d9a1
tuanhao2003 <nhtuanhao@gmail.com>
2026-01-27 15:18:30 +0700

    chore: remove useless comments in asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,32 +1,13 @@
 import os
-import threading
-import time
 import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-#redis queue auto handle thread 
 from channels.routing import ProtocolTypeRouter, URLRouter
-# from app.workers.redisQueueWorker import RedisQueueConsumer
 import app.ws_urls
 from app.middlewares.wsJwtMiddleware import WsJwtMiddleware
-
-# def redis_queue_auto_thread(queue_key='message_queue'):
-#     def runner():
-#         while True:
-#             try:
-#                 consumer = RedisQueueConsumer(queue_key=queue_key)
-#                 consumer.start()
-#                 while consumer.is_alive():
-#                     time.sleep(10)
-#             except Exception:
-#                 time.sleep(10)
-#     thread = threading.Thread(target=runner, daemon=True)
-#     thread.start()
-# if os.environ.get('RUN_MAIN') != 'true':
-#     redis_queue_auto_thread()
 
 # websocket channel
 asgi_app = get_asgi_application()
