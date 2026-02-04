75188b3d53870964ed5b6ee58cae553ae27eb27b
tuanhao2003 <nhtuanhao@gmail.com>
2026-02-04 21:48:33 +0700

    fix(middleware): fix jwt response

--- app/middlewares/jwtMiddleware.py
+++ app/middlewares/jwtMiddleware.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from app.enums.httpStatus import HttpStatus
+from app.enums.responseCodes import ResponseCodes
+from app.helpers.baseResponse import BaseResponse
 from app.helpers.exceptionHelper import ExceptionHelper
 
 
@@ -15,20 +18,20 @@ class JwtMiddleware:
 
         rawAccessToken = request.COOKIES.get("access_token")
             
-        if not rawAccessToken:    
-            ExceptionHelper.throw_unauthorized(message="Missing access token")
+        if not rawAccessToken:
+            return BaseResponse.error(status_code=HttpStatus.UNAUTHORIZED, code=ResponseCodes.INVALID_TOKEN, message="Missing access token", details="Missing access token")
 
         try:
             access_token = AccessToken(rawAccessToken)
             user_id = access_token.get("user_id")
             profile_id = access_token.get("profile_id")
             if not user_id or not profile_id:
-                ExceptionHelper.throw_unauthorized(message="Invalid token")
+                return BaseResponse.error(status_code=HttpStatus.UNAUTHORIZED, code=ResponseCodes.INVALID_TOKEN, message="Invalid token", details="Invalid token")
 
             request.user_id = user_id
             request.logging_in_profile = profile_id
             request.access_token = rawAccessToken
         except (TokenError, InvalidToken):
-            ExceptionHelper.throw_unauthorized(message="Invalid token")
+            return BaseResponse.error(status_code=HttpStatus.UNAUTHORIZED, code=ResponseCodes.INVALID_TOKEN, message="Invalid token", details="Invalid token")
 
         return self.get_response(request)
